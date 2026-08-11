84e5c9cca5f8e5587c20d420f782f6bab4783bd0
tjtyson1 <tjupnext@icloud.com>
2026-08-11 13:32:41 -0400

    Update resolver.js

--- server/services/resolver.js
+++ server/services/resolver.js
@@ -6,20 +6,30 @@ const cache = new Map();
 function resolveStreamUrl(videoId) {
     return new Promise((resolve, reject) => {
 
-        // if (cache.has(videoId)) {
-        //     return resolve(cache.get(videoId));
-        // }
         const cached = cache.get(videoId);
 
-                if (cached && cached.expires > Date.now()){
-                    return resolve(cached.url);
-                }
-                cache.delete(videoId)
+        if (cached && cached.expires > Date.now()) {
+            return resolve(cached.url);
+        }
+
+        if (cached) {
+            cache.delete(videoId);
+        }
 
         const url = `https://www.youtube.com/watch?v=${videoId}`;
-       const cmd = path.join(process.cwd(), "yt-dlp");
+
+        const ytDlp = path.join(process.cwd(), "yt-dlp");
+
         execFile(
-            cmd,["--js-runtimes", "deno", "-f", "ba", "-g", url],
+            ytDlp,
+            [
+                "--js-runtimes",
+                "deno",
+                "-f",
+                "ba",
+                "-g",
+                url
+            ],
             {
                 env: {
                     ...process.env,
@@ -29,39 +39,35 @@ function resolveStreamUrl(videoId) {
             (err, stdout, stderr) => {
 
                 if (err) {
+                    console.error("yt-dlp error:");
                     console.error(stderr || err.message);
-                    return reject(new Error("Failed to get stream"));
+
+                    return reject(
+                        new Error("Failed to get stream")
+                    );
                 }
 
                 const streamUrl = stdout.trim();
-                console.log(streamUrl)
+
+                console.log("Stream URL:", streamUrl);
+
                 if (!streamUrl) {
-                    return reject(new Error("No stream URL returned"));
+                    return reject(
+                        new Error("No stream URL returned")
+                    );
                 }
 
-                cache.set(videoId, 
-                    {
+                cache.set(videoId, {
                     url: streamUrl,
-                expires: Date.now() + 1000 * 60 * 10
+                    expires: Date.now() + 10 * 60 * 1000
                 });
 
-                setInterval(() => {
-                    const now = Date.now();
-
-                    for (const [videoId, data] of cache.entries()) {
-                        if (data.expires <= now) {
-                            cache.delete(videoId);
-                        }
-                    }
-}, 60 * 1000); // every minute
-
-                resolve(streamUrl)
-
-                
+                resolve(streamUrl);
             }
         );
-   }); 
+    });
 }
+
 module.exports = {
     resolveStreamUrl
-}
+};
